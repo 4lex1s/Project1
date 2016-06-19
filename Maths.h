9cd1218c9ec41afa9feb3c7b864be08bf6a2bb81
4lex1s <4lex1sdej@gmail.com>
2016-06-19 19:23:31 +0200

    Create Maths.h

---
+++ Maths.h
@@ -0,0 +1,10 @@
+#ifndef MATHS_H_INCLUDED
+#define MATHS_H_INCLUDED
+
+#include <SFML/Graphics.hpp>
+
+float Distance(sf::Vector2f point1, sf::Vector2f point2);
+
+float valeurAbsolue(float valeur);
+
+#endif // MATHS_H_INCLUDED
